b60d38ccb7f391474b2584134610d69b6c65093a
AlexeyShok <Alexey.Shokarev@gmail.com>
2022-02-04 14:29:00 +0600

    Added country model

---
+++ HappyTravel.KomoroContracts/Statics/Country.cs
@@ -0,0 +1,8 @@
+﻿namespace HappyTravel.KomoroContracts.Statics;
+
+public class Country
+{
+    public int Id { get; set; }
+    public string Alpha2Code { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+}
