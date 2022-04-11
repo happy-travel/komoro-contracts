965f257d8f78bf815596febb880f494e78112442
AlexeyShok <Alexey.Shokarev@gmail.com>
2022-04-12 00:52:10 +0600

    Fixed AvailabilityRestriction model

--- HappyTravel.KomoroContracts/Availabilities/AvailabilityRestriction.cs
+++ HappyTravel.KomoroContracts/Availabilities/AvailabilityRestriction.cs
@@ -3,10 +3,10 @@
 public record class AvailabilityRestriction
 {
     public int Id { get; init; }
-    public int PropertyId { get; init; }
+    public string PropertyCode { get; init; } = string.Empty;
     public DateOnly StartDate { get; init; }
     public DateOnly EndDate { get; init; }
-    public int RoomTypeId { get; init; }
+    public string RoomTypeCode { get; init; } = string.Empty;
     public string RatePlanCode { get; init; } = string.Empty;
     public RestrictionStatus? RestrictionStatus { get; init; }
     public LengthOfStay? LengthOfStay { get; init; }
