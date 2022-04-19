732d9d9cf7219f02a7f19354ff005c2ddf3bf60c
AlexeyShok <Alexey.Shokarev@gmail.com>
2022-04-20 00:26:29 +0600

    Added ErrorDetails

---
+++ HappyTravel.KomoroContracts/ErrorDetails.cs
@@ -0,0 +1,9 @@
+﻿using HappyTravel.KomoroContracts.Enums;
+
+namespace HappyTravel.KomoroContracts;
+
+public record ErrorDetails
+{
+    public ErrorCodes ErrorCode { get; init; }
+    public string? ObjectCode { get; init; }
+}
