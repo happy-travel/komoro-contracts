e93e3cf3ee2d7fcb1193cb80a9fab2e03c4f9c06
AlexeyShok <Alexey.Shokarev@gmail.com>
2022-06-06 18:41:54 +0600

    Added models for Rates

---
+++ HappyTravel.KomoroContracts/Availabilities/BaseRate.cs
@@ -0,0 +1,9 @@
+﻿namespace HappyTravel.KomoroContracts.Availabilities;
+
+public record BaseRate
+{
+    public decimal AmountBeforeTax { get; init; }
+    public decimal AmountAfterTax { get; init; }
+    public int? NumberOfGuests { get; init; }
+    public int? AgeQualifyingCode { get; init; }
+}
---
+++ HappyTravel.KomoroContracts/Availabilities/Rate.cs
@@ -0,0 +1,8 @@
+﻿namespace HappyTravel.KomoroContracts.Availabilities;
+
+public record Rate
+{
+    public string SupplierCode { get; init; } = string.Empty;
+    public string PropertyCode { get; init; } = string.Empty;
+    public List<RatePlan> RatePlans { get; init; } = new(0);
+}
