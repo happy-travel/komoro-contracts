ecc4269bfaeb22584521651b0af224dbfc62e41a
AlexeyShok <Alexey.Shokarev@gmail.com>
2022-08-03 14:21:09 +0600

    Added check-in and cjeck-out dates to BookingRequest

--- HappyTravel.KomoroContracts/Bookings/BookingRequest.cs
+++ HappyTravel.KomoroContracts/Bookings/BookingRequest.cs
@@ -3,5 +3,7 @@
 public record BookingRequest
 {
     public string ReferenceCode { get; init; } = string.Empty;
+    public DateOnly CheckInDate { get; init; }
+    public DateOnly CheckOutDate { get; init; }
     public List<RoomOccupation> Rooms { get; init; } = null!;
 }
